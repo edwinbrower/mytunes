c8bc225ed631813b4fbe68b6923f942394ac0f95
edwinbrower <edwin.brower@gmail.com>
2017-02-20 23:08:47 -0800

    Fixed syntax songQueue.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,20 +4,16 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-    debugger;
     console.log(this);
     this.on('add', function() {
       if (this.length === 1) {
-        // this.model.set('currentSong', this.at(0));
-        // this.playFirst('currentSong');
         this.playFirst();
       }
     }, this);
     this.on('ended', function() {
       this.at(0).dequeue();
     }, this);
-    // remove should take parameters. 
-    // dequeue should not
+
     this.on('dequeue', function() {
       this.remove(this.at(0));
       if (this.length > 0) {
@@ -28,10 +24,4 @@ var SongQueue = Backbone.Collection.extend({
   playFirst: function() {
     this.at(0).play();
   },
-  // dequeue: function() {
-  //   this.remove();
-  //   // this.remove(this.at(0));
-  // }
 });
-
-// comparator  ??
