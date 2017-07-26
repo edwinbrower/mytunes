ae9c7c09043c992bc27aa20726d904abcbc5d743
Edwin Brower <edwin.brower@gmail.com>
2017-07-25 20:05:26 -0700

    clean up lib view code

--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -8,7 +8,6 @@ var LibraryView = Backbone.View.extend({
     this.collection.on('sync', this.render, this);
   },
 
-
   render: function() {
     // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
     // see http://api.jquery.com/detach/
